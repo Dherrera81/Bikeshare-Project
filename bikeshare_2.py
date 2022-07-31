779ad14fcf4daee28bfc64997785bb9395b80af9
Daniel Herrera <herrera_daniel@hotmail.com>
2022-07-30 21:52:07 -0400

    Change to comments in time dif stats

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -51,13 +51,13 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # display the most common month
+    # the most common month is....
 
 
-    # display the most common day of week
+    # the most common day of week
 
 
-    # display the most common start hour
+    # the most common start hour
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
